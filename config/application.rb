0ffa9b8731820149ce7f9d7539c0205a31355311
Onepiece2424 <[onepiece2424@gmail.com]>
2021-05-02 00:15:39 +0000

    コミット1回目

--- config/application.rb
+++ config/application.rb
@@ -18,5 +18,6 @@ module HotelReservationSite
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+    config.hosts << ".amazonaws.com"
   end
 end
